2bce7bfbc8ca18b53060ca181e30f5b0c255e0cf
batmhannJ <reyeshannahjoy82@gmail.com>
2024-10-31 22:33:25 +0800

    old

--- Tienda-Seller-main/vite.config.js
+++ Tienda-Seller-main/vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173, // Use the PORT from environment or fallback to 4173
-  },
-  preview: {
-    host: true,
-    port: 5173
-  }
 })
