0c36b8511097f02d9cc3eeab776924a7ad1f3c2e
batmhannJ <reyeshannahjoy82@gmail.com>
2024-11-05 05:58:36 +0800

    seller vite

--- Tienda-Seller-main/vite.config.js
+++ Tienda-Seller-main/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ip-tienda-seller.onrender.com/', // i-update ang base URL ayon sa deployment path sa Render
   plugins: [react()],
   server: {
     host: true, // Enable this to expose
