bc649abf91872774010cd334708daa4146270bc1
batmhannJ <reyeshannahjoy82@gmail.com>
2024-11-05 08:23:50 +0800

    SELLER PATH

--- Tienda-Seller-main/src/App.jsx
+++ Tienda-Seller-main/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/login" element={!isAuthenticated ? <SLoginSignup /> : <Navigate to="/seller/addproduct" />} />
       <Route
-        path="/seller/*"
+        path="/*"
         element={
           isAuthenticated ? (
             <>
