c1a9e7d69f9b84dd68cb8f437235702e0f14b3cc
batmhannJ <reyeshannahjoy82@gmail.com>
2024-11-18 15:56:14 +0800

    patch in methods

--- Tienda-backend-main/index.js
+++ Tienda-backend-main/index.js
@@ -125,7 +125,7 @@ const upload = multer({ storage: storage });
 // Creating Upload Endpoints for Images
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified methods
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS'); // Allow specified methods
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specified headers
   next();
 });
